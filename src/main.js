9e83beb9eb37a23278f1856342062b4c41f13a38
warham1 <yellow9697@hanyang.ac.kr>
2020-08-05 21:32:17 +0900

    apollo님 죄송합니다

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router/index";
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
 new Vue({
-	render: (h) => h(App),
-	router,
+    render: (h) => h(App),
+    apolloProvider: createProvider(),
+    router
 }).$mount("#app");

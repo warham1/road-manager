4fcb8341d5ec64fa765e09b8c7bde69d7e8d703e
warham1 <yellow9697@hanyang.ac.kr>
2020-08-10 17:21:37 +0900

    버튼 정렬

--- src/components/navigate.vue
+++ src/components/navigate.vue
@@ -1,12 +1,12 @@
 <template>
-	<div>
+	<v-container class="fill-height" fluid justify-center>
 		<v-btn x-large rounded to="/manage">
 			manage
 		</v-btn>
 		<v-btn x-large rounded to="/drive">
 			drive
 		</v-btn>
-	</div>
+	</v-container>
 </template>
 
 <script>

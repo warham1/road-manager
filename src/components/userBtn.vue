1c6e3d484b74bc7b82790fd03ada5cc1d42d9297
warham1 <yellow9697@hanyang.ac.kr>
2020-08-13 19:31:21 +0900

    drive : dialog 추가

---
+++ src/components/userBtn.vue
@@ -0,0 +1,73 @@
+<template>
+	<v-btn :color="color" @click="btnOnClick">
+		{{ address }}
+		<v-dialog v-model="dialog">
+			<v-card>
+				<v-toolbar>
+					<v-toolbar-title>
+						{{ address }}
+					</v-toolbar-title>
+					<v-spacer></v-spacer>
+					<v-btn icon @click="dialog = false">
+						<v-icon>close</v-icon>
+					</v-btn>
+				</v-toolbar>
+				<v-list subheader>
+					<v-list-item-group multiple v-model="selected" color="green">
+						<v-list-item
+							v-for="(item, index) in userData[1]"
+							:key="index"
+							color="'green'"
+							@click="listsOnClick"
+						>
+							<v-list-item-icon>
+								<v-icon v-text="'account_circle'"></v-icon>
+							</v-list-item-icon>
+							<v-list-item-content>
+								<v-list-item-title v-text="item.name"></v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+			</v-card>
+		</v-dialog>
+	</v-btn>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			color: "white",
+			dialog: false,
+			selected: [],
+		};
+	},
+	props: ["click", "userData"],
+	methods: {
+		btnOnClick: function() {
+			this.dialog = true;
+		},
+		listsOnClick: function() {
+			if (this.selected.length + 1 === this.userData[1].length) this.color = "green";
+			else this.color = "yellow";
+		},
+	},
+	computed: {
+		address: function() {
+			return this.userData[0];
+		},
+	},
+	mounted() {
+		for (const [key, value] of Object.entries(this.userData[1])) {
+			if (value.name === "ning" && !this.selected.includes(key)) {
+				this.selected.push(Number(key));
+				if (this.selected.length === this.userData[1].length) this.color = "green";
+				else this.color = "yellow";
+			}
+		}
+	},
+};
+</script>
+
+<style scoped></style>
--- src/view/drive.vue
+++ src/view/drive.vue
@@ -1,12 +1,75 @@
 <template>
-  <div>
-    <p>drive view</p>
-  </div>
+	<v-container class="fill-height" align-start>
+		<v-container class="header d-flex" justify-center align-center>
+			<v-btn icon class="arrow-btn">
+				<v-icon x-large>keyboard_arrow_left </v-icon>
+			</v-btn>
+			<h2>오후 3:00 ~ 오후 4:00</h2>
+			<v-btn icon class="arrow-btn">
+				<v-icon x-large>keyboard_arrow_right</v-icon>
+			</v-btn>
+		</v-container>
+		<v-container class="content d-flex" justify-center align-start flex-wrap>
+			<userBtn v-for="(user, index) in users" :key="index" :user-data="user" :style="btnStyle"> </userBtn>
+			<p>{{ users }}</p>
+		</v-container>
+	</v-container>
 </template>
 
 <script>
-export default {};
+import userBtn from "../components/userBtn";
+
+export default {
+	data() {
+		return {
+			btnStyle: {
+				width: "40%",
+				height: "140px",
+				margin: "10px",
+			},
+			users: [],
+			dialog: false,
+			dialogData: {
+				title: "title",
+				users: [],
+			},
+		};
+	},
+	components: {
+		userBtn,
+	},
+	methods: {},
+	mounted() {
+		const url = "https://rm-graphql-server.herokuapp.com/User";
+
+		this.$http.get(url).then((result) => {
+			let temp = new Map();
+			result.data.forEach((user) => {
+				if (temp.has(user.address)) {
+					temp.get(user.address).push(user);
+				} else {
+					temp.set(user.address, [user]);
+				}
+			});
+			this.users = Array.from(temp);
+			console.table(this.users);
+		});
+	},
+};
 </script>
 
 <style>
+.header {
+	height: 50px;
+}
+
+.arrow-btn {
+	margin: 10px;
+}
+
+@media (max-width: 420px) {
+	h2 {
+		font-size: 20px;
+	}
+}
 </style>
